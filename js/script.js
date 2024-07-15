c708ec11f8340be1d558b3af2d429ec1a2f271a0
symoon96 <mono4682@gmail.com>
2024-07-16 01:43:19 +0900

    수정

--- js/script.js
+++ js/script.js
@@ -9,4 +9,30 @@ $(document).ready(function(){
             delay: 2500,
         },
     });
+
+    dateFormatChange()
 })
+
+
+
+function dateFormatChange(){
+    $('.date input[type="date"]').change(function(){
+        let date = $(this).val();
+        let dateYear = date.substr(0, 4);
+        let dateMonth = date.substr(5, 2);
+        let dateDay = date.substr(8, 2);
+
+        $(this).closest('.date').find('input[type="text"]').val(dateYear + '.' + dateMonth + '.' + dateDay)
+    });
+}
+
+function openDubleModal(id, el){
+    let target = $(id)
+    let thisEl = $(el).closest('.modal')
+
+    console.log(thisEl)
+
+    target.modal('show')
+    thisEl.css('z-index', 1054)
+    $('.modal-backdrop').last().css('z-index', 1054)
+}
